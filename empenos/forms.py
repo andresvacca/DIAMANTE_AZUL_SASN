953bb6e3d93f28ac6ce68086e0b8095c7aaed940
andresvacca <andresfvacca2019@gmail.com>
2026-04-20 22:53:22 -0500

    Update the js from empenos  crear.html

--- empenos/forms.py
+++ empenos/forms.py
@@ -64,9 +64,19 @@ class EmpenoForm(forms.ModelForm):
             if articu and str(articu.categoria) == "Oro":
                 limite = articu.precio_sugerido_venta * 0.45
                 if monto > limite:
-                    self.cleaned_data['tipo_contrato']
+                    self.cleaned_data['tipo_contrato'] = "Oro Maximo"
+                else:
+                    self.cleaned_data['tipo_contrato'] = "Contrato sobre Oro"
             else:
                 limite = articu.precio_sugerido_venta * 0.40
+                if monto > limite:
+                    self.cleaned_data['tipo_contrato'] = "Plazo Maximo"
+                else:
+                    self.cleaned_data['tipo_contrato'] = "Normal"
+                    
+        return self.cleaned_data   
+                
+                
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
