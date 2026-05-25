973c69023a9c71dcdb55b458ed10bb8f2b91aeb0
andresvacca <andresfvacca2019@gmail.com>
2026-05-24 20:37:32 -0500

    intentos de test

--- usuarios/tests.py
+++ usuarios/tests.py
@@ -7,11 +7,20 @@ class UsuarioTest(TestCase):
         self.rol_admin = Rol.objects.create(nombre="Administrador")
         self.rol_empleado = Rol.objects.create(nombre="Empleado")
         self.rol_cliente = Rol.objects.create(nombre="Cliente")
+        self.password_hash = "Pablito123!"
         
         self.usuario = Usuario.objects.create(
             nombre = "Juan Guillermo Vacca",
             email = "juanvacca@gmail.com",
-           
+            password_hash = self.password_hash,
+            id_rol=self.rol_admin,
         )
         
         return 
+    
+    def test_creacion_usuario(self):
+        self.assertEqual(self.usuario.nombre, "Juan Guillermo Vacca"),
+        self.assertEqual(self.usuario.id_rol.nombre, "Administrador"),# Verificamos la relación
+        self.assertEqual(self.usuario.email, "juanvacca@gmail.com"),
+        
+        self.assertTrue(Usuario.objects.filter(email="juanvacca@gmail.com").exists())
